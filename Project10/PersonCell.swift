9c831fd712d3759cc19cd14e5a87db455663110d
rcebul01 <roman.cebula@isobar.com>
2020-05-03 21:02:53 +0200

    Initial commit
    Add UserDefault functionality

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Roman Cebula on 28/04/2020.
+//  Copyright © 2020 Roman Cebula. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var name: UILabel!
+  
+}
